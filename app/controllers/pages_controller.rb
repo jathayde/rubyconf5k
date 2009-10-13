9605bd69eb7dadc6e4485bcbecbefaa900a35d73
John Athayde <john@hyphenateme.com>
2009-10-13 17:35:20 -0400

    more base pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,15 @@
 class PagesController < ApplicationController
+  
+  before_filter :set_page_title
+  before_filter :set_body_id
+  skip_before_filter :set_body_id, :only => :index # Keeps the background image off the home page
+  
+  
+  
+  
+  
+  private
+  def set_body_id
+    @body_id = "inside"
+  end
 end
