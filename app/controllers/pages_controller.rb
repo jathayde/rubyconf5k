52491bab6a1a5936cddd62bf4b3ff65a69f52aa0
John Athayde <john@hyphenateme.com>
2009-10-13 17:48:35 -0400

    adding SIFR and blank pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,32 @@
 class PagesController < ApplicationController
   
-  before_filter :set_page_title
   before_filter :set_body_id
   skip_before_filter :set_body_id, :only => :index # Keeps the background image off the home page
   
+  def index
+    @page_title = "Home"
+  end
   
+  def location
+    @page_title = "Where is it at?"
+  end
   
+  def about
+    @page_title = "About Us"
+  end
+  
+  def register
+    @page_title = "Registration"
+  end
+  
+  def schedule
+    @page_title = "Schedule"
+  end
   
   
   private
   def set_body_id
     @body_id = "inside"
   end
+  
 end
