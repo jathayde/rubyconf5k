b7ed4a4e8f381881f30c55235ff6e3f5c5505067
David Chelimsky <dchelimsky@gmail.com>
2009-10-22 13:10:53 -0500

    fixed some FIXME WITH LINKs

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,6 @@
 module PagesHelper
+  def link_to_charity
+    link_to "The Leukemia &amp; Lymphoma Society", 
+      "http://www.guidestar.org/pqShowGsReport.do?partner=justgivews&ein=13-5644916"
+  end
 end
