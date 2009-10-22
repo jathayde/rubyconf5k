e9c7bfc9f5d3fb49fe6f1783dd5e900dbe18b467
David Chelimsky <dchelimsky@gmail.com>
2009-10-22 12:52:37 -0500

    redirect unknown actions and routing errors to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  rescue_from ActionController::UnknownAction, ActionController::RoutingError do
+    redirect_to root_url
+  end
 end
