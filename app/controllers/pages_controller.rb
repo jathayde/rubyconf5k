d29ed5fe9275fee2a6bcafa2ad46be619c88345a
David Chelimsky <dchelimsky@gmail.com>
2009-10-22 12:53:17 -0500

    remove about stuff and clean up some text

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,36 +1,28 @@
 class PagesController < ApplicationController
-  
-  
+
   def index
     @body_id = "home"
     @page_title = "Home"
   end
-  
+
   def location
     @body_id = "inside"
     @page_title = "Where is it at?"
   end
-  
-  def about
-    @body_id = "inside"
-    @page_title = "About Us"
-  end
-  
+
   def register
     @body_id = "inside"
     @page_title = "Registration"
   end
-  
+
   def schedule
     @body_id = "inside"
     @page_title = "Schedule"
   end
-  
+
   def license
     @body_id = "inside"
     @page_title = "License"
   end
-  
-  
-  
+
 end
