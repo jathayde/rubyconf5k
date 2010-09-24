51b0e252c530d0fd8f93e1800bccbecde171470f
Tom Copeland <tom@infoether.com>
2010-09-23 22:48:07 -0400

    Initial results of 'rails new .', routes file converted using rails-upgrade.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyconf5k::Application.config.secret_token = '49916c6c9064dc44ab892971d9635d808c715c5e98641adaea96dc9ec07b6fdd7450bfdca06c332796a2d1a84101e9e8bfef037be0f46c4e1dabd345e7081898'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_rubyconf5k_session',
-  :secret      => '49916c6c9064dc44ab892971d9635d808c715c5e98641adaea96dc9ec07b6fdd7450bfdca06c332796a2d1a84101e9e8bfef037be0f46c4e1dabd345e7081898'
-}
+Rubyconf5k::Application.config.session_store :cookie_store, :key => '_rubyconf5k_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Rubyconf5k::Application.config.session_store :active_record_store
